403932a946dfffc08719855820de6abe7823ea97
Rakesh Ranjan Jena <rakeshranjanjena7@gmail.com>
2022-11-21 20:22:38 +0530

    listing updated

--- resources/views/admin/article/listing.blade.php
+++ resources/views/admin/article/listing.blade.php
@@ -42,8 +42,8 @@
                                     <?=date_format(date_create($article->created_at), "d M, Y")?>
                                 </td>
                                 <td>
-                                    <a href="{{ url('article') }}/{{ $article->id }}/{{ $article->slug }}" class="btn btn-primary rounded-pill btn-sm">View</a>
-                                    <a href="{{ url('admin/articles') }}/{{ $article->id }}/edit"
+                                    <a href="{{ url('/article') }}/{{ $article->id }}/{{ $article->slug }}" class="btn btn-primary rounded-pill btn-sm">View</a>
+                                    <a href="{{ url('/articles') }}/{{ $article->id }}/edit"
                                         class="btn btn-primary rounded-pill btn-sm">Edit</a>
                                     <a href="{{ url('admin/articles') }}/{{ $article->id }}"
                                         class="btn btn-danger rounded-pill btn-sm">Delete</a>
